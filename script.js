3b61206dd87fd681d1bbfbe46b254e02cdf20fb8
Thiago Vinicius <88091178+Thiagox47@users.noreply.github.com>
2023-02-10 10:40:50 -0300

    Atualização da calculadora
    Esta atualização foi com ajuda do ChatGPT e algumas alterações foram realizadas para funcionamento do código.
    
    Basicamente foi apenas simplificado e alterados os IDs para Inglês

--- script.js
+++ script.js
@@ -1,74 +1,51 @@
-//Variavel Global do paragrafo da operação.
-let p1 = document.getElementById("p1");
+// Variável global do parágrafo da operação.
+let p1 = document.getElementById("operation");
 
-//Variavel da resposta e variavel da soma.
-let res = document.getElementById('resp');
+// Variáveis da resposta e da soma.
+let res = document.getElementById('result');
 
-//Funções que realizam a alteração do caractere para operação.
-function soma() {
-    p1.innerHTML = '+';
-    document.getElementById('num1').style.display = "block";
+// Função que realiza a alteração do caractere para operação.
+function changeOperation(operacao) {
+    p1.innerHTML = operacao;
+    document.getElementById('num1').style.display = operacao !== '√' ? "block" : "none";
 }
 
-function subtracao() {
-    p1.innerHTML = '-';
-    document.getElementById('num1').style.display = "block";
-}
-
-function multiplicacao() {
-    p1.innerHTML = '*';
-    document.getElementById('num1').style.display = "block";
-}
-
-function divisao() {
-    p1.innerHTML = '/';
-    document.getElementById('num1').style.display = "block";
-}
-
-function raiz() {
-    p1.innerHTML = '√';
-    document.getElementById('num1').style.display = "none";
-}
-
-function porcentagem() {
-    p1.innerHTML = '%';
-    document.getElementById('num1').style.display = "block";
-}
-
-//Função que realiza o calculo.
-function calcular() {
-    //Variaveis dos inputs.
+// Função que realiza o cálculo.
+function calculate() {
+    // Variáveis dos inputs.
     let n1 = Number(document.getElementById('num1').value);
     let n2 = Number(document.getElementById('num2').value);
-    //Verificar se há dados inseridos nos inputs e se foi selecionada a operação.
-    if (p1.innerHTML == "" | n1 == "" & n2 == "") {
-        alert("Os valores devem ser adicionados e a operação deve ser escolhida, verifique se está tudo certo antes de continuar!")
-    } else {
-        //Variavel da resposta e switch que identifica qual operação está sendo utilizada.
-        let s;
-        switch (p1.innerHTML) {
-            case '+':
-                s = n1 + n2
-                break
-            case '-':
-                s = n1 - n2
-                break
-            case '*':
-                s = n1 * n2
-                break
-            case '/':
-                s = n1 / n2
-                break
-            case '√':
-                s = Math.sqrt(n2)
-                break
-            case '%':
-                s = (n1 / 100) * n2
-                break
-            default:
-                break
-        }
-        //Mostra o resultado.
-        res.innerHTML = `O resultado é: ${s}`
+    // Verificar se há dados inseridos nos inputs e se foi selecionada a operação.
+    if (!p1.innerHTML || (isNaN(n1) && isNaN(n2)) || (n1 == '' && n2 == '')) {
+        alert("Os valores devem ser adicionados e a operação deve ser escolhida, verifique se está tudo certo antes de continuar!");
+        return;
     }
+
+    // Variável da resposta e switch que identifica qual operação está sendo utilizada.
+    let s;
+    switch (p1.innerHTML) {
+        case '+':
+            s = n1 + n2;
+            break;
+        case '-':
+            s = n1 - n2;
+            break;
+        case '*':
+            s = n1 * n2;
+            break;
+        case '/':
+            s = n1 / n2;
+            break;
+        case '√':
+            s = Math.sqrt(n2);
+            break;
+        case '%':
+            s = (n1 / 100) * n2;
+            break;
+        default:
+            break;
+    }
+
+    // Mostra o resultado.
+    res.innerHTML = `O resultado é: ${s}`;
 }
